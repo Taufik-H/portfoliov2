15d60a9095d2f3bfe3264044df648011902ce4b3
Taufik-H <taufikrpl1@gmail.com>
2023-12-01 18:11:49 +0700

    new const data testimonials

--- constant/index.ts
+++ constant/index.ts
@@ -38,3 +38,38 @@ export const SOCIAL_MEDIAS = [
     icon: "/github.svg",
   },
 ];
+
+export const Testimoni = [
+  {
+    id: 1,
+    name: "John Doe",
+    title: "UI/UX Designer",
+    image: "/user1.png",
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptates.",
+  },
+  {
+    id: 2,
+    name: "Jenita",
+    title: "Web Developer",
+    image: "/user2.png",
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptates  adipisicing elit. Quisquam, voluptates.",
+  },
+  {
+    id: 3,
+    name: "Samuel Alexandra",
+    title: "UI/UX Designer",
+    image: "/user3.png",
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptates. adipisicing elit. Quisquam, voluptates adipisicing elit. Quisquam, voluptates",
+  },
+  {
+    id: 4,
+    name: "Alex Napoleon",
+    title: "Web Developer",
+    image: "/user4.png",
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, adipisicing elit. Quisquam, voluptates adipisicing elit. Quisquam, voluptates adipisicing elit. Quisquam, voluptates voluptates.",
+  },
+];
