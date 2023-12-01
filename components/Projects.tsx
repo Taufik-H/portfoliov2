bd2d6b475e1bbbee4703f78483d0fe4414421f91
Taufik-H <taufikrpl1@gmail.com>
2023-12-01 18:11:24 +0700

    chore:projects layouting

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -8,25 +8,10 @@ const Projects = () => {
   return (
     <div className="bgProjects max-container padding-container py-10">
       <PageTitle subtitle="Recent Projects" title="projects" />
-      <div className="my-10 flex flex-col lg:flex-row justify-around items-center">
-        <CardProject
-          image="/raimu.png"
-          desc="AI imege generator build with ReactJs,TailwindCss"
-          icon="/eye.svg"
-          title="raimu.pro"
-        />
-        <CardProject
-          image="/iitc.png"
-          desc="Competition management build with NextJs, Laravel"
-          icon="/eye.svg"
-          title="iit competition"
-        />
-        <CardProject
-          image="/edufree.png"
-          desc="LMS Landing page build with ViteJs, TailwindCss"
-          icon="/eye.svg"
-          title="EDUFREE"
-        />
+      <div className="my-10 flex flex-col gap-5 lg:flex-row justify-center items-center">
+        <CardProject image="/raimu.png" title="raimu.pro" alt="raimu" />
+        <CardProject image="/iitc.png" title="iit competition" alt="iitc" />
+        <CardProject image="/edufree.png" title="EDUFREE" alt="edufree" />
       </div>
     </div>
   );
